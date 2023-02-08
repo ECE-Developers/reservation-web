bce1a04c3e385968b4944d6ece2a8b6c132e473d
bala <balamogoulish@naver.com>
2023-02-09 02:13:22 +0900

    fix: 빌드 에러 수정

--- src/Page/MainPage.js
+++ src/Page/MainPage.js
@@ -1,9 +1,9 @@
 /**
  * To Do List MainPage
- * - api 파싱 후 사용자의 예약 내역을 '00월 00일 00시 테이블0 <예약취소>' 형식으로 구현
- * - 예약 취소 버튼 클릭 시 api로 예약 삭제 내역을 찌르고 새로고침
+ * - 임의 데이터 만들기
+ * - 사용자 예약 내역 받아서 초록색으로 표시할 수 있어야 함!!
  */
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import HeaderLogin from '../layout/HeaderLogin';
 import {useNavigate} from "react-router-dom"
 import moment from 'moment';
@@ -11,22 +11,11 @@ import '../css/Table.css'
 
 function MainPage(){
   const [selectedTable, setSelectedTable] = useState('Table1');
-  const [noButton, setNoButton] = useState(true);
-  const [selectedTime, setSelectedTime] = useState({});
-  const [selectedCount, setSelectedCount] = useState(0);
   const today = moment().format('MM-DD');
   const tomorrow = moment().add(1, 'days').format('MM-DD');
   const dayAfterTomorrow = moment().add(2, 'days').format('MM-DD');
   const days = [today, tomorrow, dayAfterTomorrow];
   const times = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
-  useEffect(() => {
-    if(selectedCount>0){
-      setNoButton(false)
-      return;
-    }
-    setNoButton(true);
-  },[selectedCount]);
 
   const handleTableSelection = table => {
     setSelectedTable(table);
@@ -60,12 +49,7 @@ function MainPage(){
               <tr key={time}>
                 <td>{time} - {time+1}</td>
                 {days.map(day => (
-                  <td
-                    key={day}
-                    className={selectedTime[day]?.[time] ? 'booked' : ''}
-                  >
-                    {selectedTime[day]?.[time] ? '' : ''}
-                  </td>
+                  <td></td>
                 ))}
               </tr>
             ))}
