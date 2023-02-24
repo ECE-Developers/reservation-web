8aa1aeaf1d1eca77bff6485f1950de6247efe6a5
bala <balamogoulish@naver.com>
2023-02-24 14:15:22 +0900

    fix: 빌드 에러 수정

--- src/layout/HeaderLogin.js
+++ src/layout/HeaderLogin.js
@@ -9,12 +9,6 @@ const HeaderLogin =() => {
     navigate("/main");
   }
 
-  const onClickLogOut = () => {
-    localStorage.removeItem(`id`);
-    localStorage.removeItem(`token`);
-    navigate('/')
-  }
-
   return(
     <header className='header'>
       <div>
